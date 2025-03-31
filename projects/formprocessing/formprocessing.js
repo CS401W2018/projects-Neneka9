8200fbc9c20ec7eeebe2f25fa60ea2d89c5c6315
Neneka Hayashi <nenekkuma927@icloud.com>
2025-03-31 11:52:33 -1000

    add

---
+++ projects/formprocessing/formprocessing.js
@@ -0,0 +1,53 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Form Submitted");
+
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    const interests = document.getElementById('interests').value;
+    const age = document.getElementById('age').value;
+
+    if (!firstname) {
+        alart("You need to enter your first name.");
+        return;
+    }
+
+    if (!lastname) {
+        alart("You need to enter your last name.");
+        return;
+    }
+
+    if (!interests === "—") {
+        alart("You need to choose your interests.");
+        return;
+    }
+
+    if (!age || age<18) {
+        alart("You need to be 18");
+        return;
+    }
+
+    const formData = {
+        firstname: firstname,
+        email: email,
+        password: pass,
+        state: document.getElementById('state').value,
+    };
+
+    console.log(formData);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Constent-Type", "application/json;charset=UTF-8") ;
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+    console.log(formData)
+
+});
